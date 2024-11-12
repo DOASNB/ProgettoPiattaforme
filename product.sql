3a37f79212c2bcc500507adfc415d37cac2d50c8
DOASBN <marco.tronchi@virgilio.it>
2024-11-12 20:13:44 +0100

    first commit.

---
+++ product.sql
@@ -0,0 +1,6 @@
+INSERT INTO store.product (id, name, code, description, price, quantity, image_name, version) VALUES (6, 'Decorazione Marina', '634563', 'decorazione in legno di mogano fatta a mano', 30, 1, null, 3);
+INSERT INTO store.product (id, name, code, description, price, quantity, image_name, version) VALUES (4, 'Decorazione in legno', '4257252', 'Maschera in legno alta 20 cm', 55, 8, 'mask', 5);
+INSERT INTO store.product (id, name, code, description, price, quantity, image_name, version) VALUES (7, 'Figurina cavaliere', '854563', 'Figurina in legno di abete', 20, 9, null, 4);
+INSERT INTO store.product (id, name, code, description, price, quantity, image_name, version) VALUES (8, 'Figurina Gufo', '638797', 'Figurina di un gufo fatta a mano in legno di mogano', 24, 6, 'dragon', 3);
+INSERT INTO store.product (id, name, code, description, price, quantity, image_name, version) VALUES (2, 'Statua Coccodrillo', '562627', 'Statua di un coccodrillo in legno di mogano', 50, 0, 'coccodrillo', 7);
+INSERT INTO store.product (id, name, code, description, price, quantity, image_name, version) VALUES (1, 'Statua Robot', '263451', 'Statua robot in legno di mogano alta 15cm', 24, 14, 'robot', 11);
